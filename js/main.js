674c974dc01110b439d0d27033d3ddb26b8fcb64
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-06-27 18:32:14 +0300

    final commit2

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const carouselBtn = document.getElementsByClassName('favourite__carousel-btn')
+
+// carouselBtn[0].addEventListener('click', () => {
+//     const items = document.querySelectorAll('.favourite__item')
+//     let index = 0
+//     let needItems = []
+//     for (let item of items) {
+//         if (index % 2 !== 0) {
+//             needItems.push(item)
+//         }
+//         index += 1
+//     }
+//
+//     console.log(needItems)
+//
+//     needItems.forEach(el => {
+//         el.classList.toggle('active')
+//     })
+// })
